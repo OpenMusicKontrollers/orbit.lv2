4f4432876e1075df254c24bcb0c4ca73c24bb03b
Hanspeter Portner <dev@open-music-kontrollers.ch>
2015-08-05 22:18:32 +0200

    prototype more modes.
    * play
    * record
    * replace (play + record)
    * substitute (play + record with fixed length)

--- orbit_looper.c
+++ orbit_looper.c
@@ -24,13 +24,20 @@
 #define BUF_SIZE 0x1000000 // 16 MB
 #define BUF_PERCENT (100.f / BUF_SIZE)
 
+typedef enum _plugmode_t plugmode_t;
 typedef enum _plugstate_t plugstate_t;
 typedef struct _plughandle_t plughandle_t;
 
+enum _plugmode_t {
+	MODE_PLAY				= 0,
+	MODE_RECORD			= 1,
+	MODE_REPLACE		= 2,
+	MODE_SUBSTITUTE	= 3
+};
+
 enum _plugstate_t {
-	STATE_PLAY					= 0,
-	STATE_PAUSE					= 1,
-	STATE_RECORD				= 2
+	STATE_PAUSED		= 0,
+	STATE_ROLLING		= 1
 };
 
 struct _plughandle_t {
@@ -40,14 +47,18 @@ struct _plughandle_t {
 
 	const LV2_Atom_Sequence *event_in;
 	LV2_Atom_Sequence *event_out;
+	const float *mode;
 	const float *state;
-	float *capacity;
+	float *capacity [2];
 	float *position;
-
+	
+	plugmode_t mode_i;
 	plugstate_t state_i;
-	uint32_t cap_i;
+	uint32_t cap_i [2];
+
+	int play;
 	int64_t last_i;
-	uint8_t buf [BUF_SIZE];
+	uint8_t buf [2][BUF_SIZE];
 	int64_t offset;
 	LV2_Atom_Event *ev;
 };
@@ -94,12 +105,18 @@ connect_port(LV2_Handle instance, uint32_t port, void *data)
 			handle->event_out = (LV2_Atom_Sequence *)data;
 			break;
 		case 2:
-			handle->state = (const float *)data;
+			handle->mode = (const float *)data;
 			break;
 		case 3:
-			handle->capacity = (float *)data;
+			handle->state = (const float *)data;
 			break;
 		case 4:
+			handle->capacity[0] = (float *)data;
+			break;
+		case 5:
+			handle->capacity[1] = (float *)data;
+			break;
+		case 6:
 			handle->position = (float *)data;
 			break;
 		default:
@@ -112,111 +129,178 @@ activate(LV2_Handle instance)
 {
 	plughandle_t *handle = instance;
 
-	handle->state_i = STATE_PAUSE;
+	handle->mode_i = MODE_PLAY;
+	handle->state_i = STATE_PAUSED;
 	handle->offset = 0;
+	handle->play = 0;
+}
+
+static inline void
+_play(plughandle_t *handle, uint32_t capacity, uint32_t nsamples)
+{
+	LV2_Atom_Sequence *play_seq = (LV2_Atom_Sequence *)handle->buf[handle->play];
+
+	while(!lv2_atom_sequence_is_end(&play_seq->body, play_seq->atom.size, handle->ev))
+	{
+		if(handle->ev->time.frames >= handle->offset + nsamples)
+			break; // event not part of this period
+
+		LV2_Atom_Event *e = lv2_atom_sequence_append_event(handle->event_out,
+			capacity, handle->ev);
+		if(e)
+		{
+			e->time.frames -= handle->offset;
+			if(e->time.frames < 0)
+				e->time.frames = 0; // underflow
+		}
+		else
+			break; // overflow
+
+		handle->ev = lv2_atom_sequence_next(handle->ev);
+	}
+}
+
+static inline void
+_rec(plughandle_t *handle, int substitute)
+{
+	LV2_Atom_Sequence *rec_seq = (LV2_Atom_Sequence *)handle->buf[!handle->play];
+
+	LV2_ATOM_SEQUENCE_FOREACH(handle->event_in, ev)
+	{
+		LV2_Atom_Event *e = lv2_atom_sequence_append_event(rec_seq, BUF_SIZE, ev);
+		if(e)
+		{
+			e->time.frames += handle->offset;
+			if(!substitute)
+				handle->last_i = e->time.frames;
+		}
+		else
+			break; // overflow
+	}
+}
+
+static inline void
+_rewind_play(plughandle_t *handle)
+{
+	LV2_Atom_Sequence *play_seq = (LV2_Atom_Sequence *)handle->buf[handle->play];
+
+	// rewind
+	handle->ev = lv2_atom_sequence_begin(&play_seq->body);
+
+	handle->offset = 0;
+}
+
+static inline void
+_rewind_rec(plughandle_t *handle, int substitute)
+{
+	LV2_Atom_Sequence *rec_seq = (LV2_Atom_Sequence *)handle->buf[!handle->play];
+
+	// rewind
+	rec_seq->atom.type = handle->forge.Sequence;
+	rec_seq->atom.size = sizeof(LV2_Atom_Sequence_Body);
+	rec_seq->body.unit = 0; // frames
+	rec_seq->body.pad = 0;
+
+	handle->offset = 0;
+	if(!substitute)
+		handle->last_i = 0;
 }
 
 static void
 run(LV2_Handle instance, uint32_t nsamples)
 {
 	plughandle_t *handle = instance;
-	LV2_Atom_Sequence *seq = (LV2_Atom_Sequence *)handle->buf;
+	LV2_Atom_Sequence *play_seq = (LV2_Atom_Sequence *)handle->buf[handle->play];
+	LV2_Atom_Sequence *rec_seq = (LV2_Atom_Sequence *)handle->buf[!handle->play];
 
+	plugmode_t mode_i = floor(*handle->mode);
 	plugstate_t state_i = floor(*handle->state);
+
+	//int mode_has_changed = mode_i != handle->mode_i;
+	int state_has_changed = state_i != handle->state_i;
 
 	uint32_t capacity = handle->event_out->atom.size;
 	lv2_atom_sequence_clear(handle->event_out);
 
-	switch(state_i)
+	if(state_i == STATE_ROLLING)
 	{
-		case STATE_PAUSE:
+		if(state_has_changed)
 		{
-			// do nothing
+			handle->play ^= 1; // swap buffers
+			play_seq = (LV2_Atom_Sequence *)handle->buf[handle->play];
+			rec_seq = (LV2_Atom_Sequence *)handle->buf[!handle->play];
 
-			break;
+			_rewind_play(handle);
+			_rewind_rec(handle, mode_i == MODE_SUBSTITUTE);
+			
+			handle->mode_i = mode_i; // mode is only read upon starting to roll
 		}
-		case STATE_PLAY:
+
+		switch(handle->mode_i)
 		{
-
-			if(state_i != handle->state_i) // state has changed since last period
+			case MODE_PLAY: // only playback, no recording
 			{
-				// rewind
-				handle->ev = lv2_atom_sequence_begin(&seq->body);
+				_play(handle, capacity, nsamples);
 
-				handle->offset = 0;
+				// automatic repeat
+				if(lv2_atom_sequence_is_end(&play_seq->body, play_seq->atom.size, handle->ev))
+					_rewind_play(handle);
+
+				break;
 			}
-
-			// repeat
-			if(lv2_atom_sequence_is_end(&seq->body, seq->atom.size, handle->ev))
+			case MODE_RECORD: // no playback, only recording
 			{
-				// rewind
-				handle->ev = lv2_atom_sequence_begin(&seq->body);
+				_rec(handle, 0);
 
-				handle->offset = 0;
+				break;
 			}
-
-			while(!lv2_atom_sequence_is_end(&seq->body, seq->atom.size, handle->ev))
+			case MODE_REPLACE: // playback and recording
 			{
-				if(handle->ev->time.frames >= handle->offset + nsamples)
-					break; // event not part of this period
+				_play(handle, capacity, nsamples);
+				_rec(handle, 0);
 
-				LV2_Atom_Event *e = lv2_atom_sequence_append_event(handle->event_out,
-					capacity, handle->ev);
-				if(e)
+				break;
+			}
+			case MODE_SUBSTITUTE: // playback and recording for given region
+			{
+				_play(handle, capacity, nsamples);
+				_rec(handle, 1);
+
+				// automatic repeat
+				if(lv2_atom_sequence_is_end(&play_seq->body, play_seq->atom.size, handle->ev)
+					&& (handle->offset >= handle->last_i) )
 				{
-					e->time.frames -= handle->offset;
-					if(e->time.frames < 0)
-						e->time.frames = 0; // underflow
+					handle->play ^= 1; // switch buffers
+					play_seq = (LV2_Atom_Sequence *)handle->buf[handle->play];
+					rec_seq = (LV2_Atom_Sequence *)handle->buf[!handle->play];
+
+					_rewind_play(handle);
+					_rewind_rec(handle, 1);
 				}
-				else
-					break; // overflow
 
-				handle->ev = lv2_atom_sequence_next(handle->ev);
+				break;
 			}
-
-			break;
-		}
-		case STATE_RECORD:
-		{
-			if(state_i != handle->state_i) // state has changed since last period
-			{
-				// rewind
-				seq->atom.type = handle->forge.Sequence;
-				seq->atom.size = sizeof(LV2_Atom_Sequence_Body);
-				seq->body.unit = 0; // frames
-				seq->body.pad = 0;
-
-				handle->offset = 0;
-				handle->last_i = 0;
-			}
-
-			LV2_ATOM_SEQUENCE_FOREACH(handle->event_in, ev)
-			{
-				LV2_Atom_Event *e = lv2_atom_sequence_append_event(seq, BUF_SIZE, ev);
-				if(e)
-				{
-					e->time.frames += handle->offset;
-					handle->last_i = e->time.frames;
-				}
-				else
-					break; // overflow
-			}
-
-			break;
 		}
 	}
 
-	uint32_t cap_i = sizeof(LV2_Atom) + seq->atom.size;
-	if(cap_i != handle->cap_i)
+	uint32_t cap_i = sizeof(LV2_Atom) + play_seq->atom.size;
+	if(cap_i != handle->cap_i[0])
 	{
-		*handle->capacity = BUF_PERCENT * seq->atom.size;
-		handle->cap_i = cap_i;
+		*handle->capacity[0] = BUF_PERCENT * play_seq->atom.size;
+		handle->cap_i[0] = cap_i;
+	}
+	cap_i = sizeof(LV2_Atom) + rec_seq->atom.size;
+	if(cap_i != handle->cap_i[1])
+	{
+		*handle->capacity[1] = BUF_PERCENT * rec_seq->atom.size;
+		handle->cap_i[1] = cap_i;
 	}
 
+	handle->mode_i = mode_i;
 	handle->state_i = state_i;
 	handle->offset += nsamples;
 
-	if(state_i == STATE_PLAY)
+	if(state_i == STATE_ROLLING)
 	{
 		if(handle->offset < handle->last_i)
 			*handle->position = 100.f * handle->offset / handle->last_i;
