813775e937f0ab208b4649a3a63887bbf80051d1
Hanspeter Portner <dev@open-music-kontrollers.ch>
2015-12-01 21:19:05 +0100

    further prototype orbit:cargoship.

--- orbit_cargoship.c
+++ orbit_cargoship.c
@@ -18,30 +18,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <zlib.h>
 
 #include <orbit.h>
 #include <timely.h>
 #include <varchunk.h>
 
 #define BUF_SIZE 0x10000
+#define DEFAULT_FILE_NAME "seq.atom"
 
-typedef enum _punchmode_t punchmode_t;
 typedef enum _jobtype_t jobtype_t;
 typedef struct _job_t job_t;
 typedef struct _plughandle_t plughandle_t;
-
-enum _punchmode_t {
-	PUNCH_BEAT			= 0,
-	PUNCH_BAR				= 1
-};
 
 enum _jobtype_t {
 	JOB_PLAY				= 0,
 	JOB_RECORD			= 1,
 	JOB_SEEK 				= 2,
-	JOB_OPEN				= 3,
-	JOB_DRAIN				= 4
+	JOB_OPEN_PLAY		= 3,
+	JOB_OPEN_RECORD	= 4,
+	JOB_DRAIN				= 5
 };
 
 struct _job_t {
@@ -66,19 +61,20 @@ struct _plughandle_t {
 
 	const LV2_Atom_Sequence *event_in;
 	LV2_Atom_Sequence *event_out;
-	const float *punch;
 	const float *record;
 
-	punchmode_t punch_i;
 	bool record_i;
-	unsigned count_i;
-	int64_t offset;
+
+	double beats_period;
+	double beats_upper;
 
 	bool rolling;
 	int draining;
 
 	char path [1024];
-	gzFile io;
+	FILE *io;
+
+	double seek_beats;
 
 	varchunk_t *to_disk;
 	varchunk_t *from_disk;
@@ -122,10 +118,20 @@ _trigger_seek(plughandle_t *handle, double beats)
 }
 
 static inline LV2_Worker_Status
-_trigger_open(plughandle_t *handle)
+_trigger_open_play(plughandle_t *handle)
 {
 	const job_t job = {
-		.type = JOB_OPEN
+		.type = JOB_OPEN_PLAY
+	};
+
+	return _trigger_job(handle, &job);
+}
+
+static inline LV2_Worker_Status
+_trigger_open_record(plughandle_t *handle)
+{
+	const job_t job = {
+		.type = JOB_OPEN_RECORD
 	};
 
 	return _trigger_job(handle, &job);
@@ -144,38 +150,36 @@ _trigger_drain(plughandle_t *handle)
 static inline void
 _play(plughandle_t *handle, int64_t to, uint32_t capacity)
 {
-	const int64_t ref = handle->offset - to; // beginning of current period
-
 	const LV2_Atom_Event *src;
 	size_t len;
 	while((src = varchunk_read_request(handle->from_disk, &len)))
 	{
 		if(handle->draining > 0)
 		{
-			printf("draining: %u\n", handle->draining);
 			if(src->body.type == handle->uris.orbit_drain)
+			{
+				//fprintf(stderr, "_play: draining %i\n", handle->draining);
 				handle->draining -= 1;
+			}
 
 			varchunk_read_advance(handle->from_disk);
 			continue;
 		}
 
-		const int64_t beat_frames = src->time.beats * TIMELY_FRAMES_PER_BEAT(&handle->timely);
+		//fprintf(stderr, "_play: %lf %lf\n", src->time.beats, handle->beats_upper);
 
-		//printf("event: %lf %li | %li\n", src->time.beats, beat_frames, handle->offset);
-
-		if(beat_frames >= handle->offset)
+		if(src->time.beats >= handle->beats_upper)
 			break; // event not part of this region
 
 		LV2_Atom_Event *dst = lv2_atom_sequence_append_event(handle->event_out,
 			capacity, src);
 		if(dst)
 		{
-			dst->time.frames = beat_frames - ref;
+			dst->time.frames = (src->time.beats - handle->beats_period) * TIMELY_FRAMES_PER_BEAT(&handle->timely);
 
 			if(dst->time.frames < 0)
 			{
-				fprintf(stderr, "event late %li\n", dst->time.frames);
+				//fprintf(stderr, "_play: event late %li\n", dst->time.frames);
 				dst->time.frames = 0;
 			}
 		}
@@ -194,7 +198,7 @@ _rec(plughandle_t *handle, const LV2_Atom_Event *src)
 	size_t len = sizeof(LV2_Atom_Event) + src->body.size;
 	if((dst = varchunk_write_request(handle->to_disk, len)))
 	{
-		dst->time.beats = handle->offset / TIMELY_FRAMES_PER_BEAT(&handle->timely);
+		dst->time.beats = handle->beats_upper;
 		dst->body.type = src->body.type;
 		dst->body.size = src->body.size;
 		memcpy(LV2_ATOM_BODY(&dst->body), LV2_ATOM_BODY_CONST(&src->body), src->body.size);
@@ -205,11 +209,8 @@ _rec(plughandle_t *handle, const LV2_Atom_Event *src)
 static inline void
 _reposition_play(plughandle_t *handle, double beats)
 {
-	if(beats == 0.0)
-	{
-		handle->draining += 1;
-		_trigger_drain(handle);
-	}
+	handle->draining += 1;
+	_trigger_drain(handle);
 	_trigger_seek(handle, beats);
 }
 
@@ -217,6 +218,17 @@ static inline void
 _reposition_rec(plughandle_t *handle, double beats)
 {
 	_trigger_seek(handle, beats);
+}
+
+static inline double
+_beats(timely_t *timely)
+{
+	double beats = TIMELY_BAR(timely)
+		* TIMELY_BEATS_PER_BAR(timely)
+		+ TIMELY_BAR_BEAT(timely)
+		+ timely->offset.beat / TIMELY_FRAMES_PER_BEAT(timely);
+
+	return beats;
 }
 
 static void
@@ -230,16 +242,12 @@ _cb(timely_t *timely, int64_t frames, LV2_URID type, void *data)
 	}
 	else if(type == TIMELY_URI_BAR_BEAT(timely))
 	{
-		double beats = (double)TIMELY_BAR(timely) * TIMELY_BEATS_PER_BAR(timely)
-			+ TIMELY_BAR_BEAT(timely);
+		double beats = _beats(&handle->timely);
 
-		if(handle->punch_i == PUNCH_BEAT)
-			handle->offset = beats * TIMELY_FRAMES_PER_BEAT(timely);
-		else if(handle->punch_i == PUNCH_BAR)
-			handle->offset = beats * TIMELY_FRAMES_PER_BEAT(timely); //FIXME
-
-		//_reposition_rec(handle, beats); FIXME
-		_reposition_play(handle, beats);
+		if(handle->record_i)
+			_reposition_rec(handle, beats);
+		else
+			_reposition_play(handle, beats);
 	}
 }
 
@@ -291,20 +299,13 @@ instantiate(const LV2_Descriptor* descriptor, double rate,
 		| TIMELY_MASK_BEATS_PER_BAR
 		| TIMELY_MASK_BEATS_PER_MINUTE
 		| TIMELY_MASK_FRAMES_PER_SECOND
-		| TIMELY_MASK_SPEED
-		| TIMELY_MASK_BAR_BEAT_WHOLE;
+		| TIMELY_MASK_SPEED;
 	timely_init(&handle->timely, handle->map, rate, mask, _cb, handle);
 	lv2_atom_forge_init(&handle->forge, handle->map);
 
-	char *tmp = make_path->path(make_path->handle, "seq.gz");
+	char *tmp = make_path->path(make_path->handle, DEFAULT_FILE_NAME);
 	strcpy(handle->path, tmp);
 	free(tmp);
-
-	if(!(handle->io = gzopen(handle->path, "r")))
-	{
-		free(handle);
-		return NULL;
-	}
 	
 	handle->uris.orbit_path = handle->map->map(handle->map->handle, ORBIT_PATH_URI);
 	handle->uris.orbit_drain = handle->map->map(handle->map->handle, ORBIT_DRAIN_URI);
@@ -329,9 +330,6 @@ connect_port(LV2_Handle instance, uint32_t port, void *data)
 			handle->event_out = (LV2_Atom_Sequence *)data;
 			break;
 		case 2:
-			handle->punch = (const float *)data;
-			break;
-		case 3:
 			handle->record = (const float *)data;
 			break;
 		default:
@@ -344,9 +342,12 @@ activate(LV2_Handle instance)
 {
 	plughandle_t *handle = instance;
 
-	handle->offset = 0.f;
 	handle->rolling = false;
 	handle->restored = true;
+	handle->seek_beats = 0.0;
+
+	handle->beats_period = 0.0;
+	handle->beats_upper = 0.0;
 }
 
 static void
@@ -355,31 +356,31 @@ run(LV2_Handle instance, uint32_t nsamples)
 	plughandle_t *handle = instance;
 	uint32_t capacity = handle->event_out->atom.size;
 
-	punchmode_t punch_i = floor(*handle->punch);
-	bool punch_has_changed = punch_i != handle->punch_i;
-	handle->punch_i = punch_i;
-
 	bool record_i = floor(*handle->record);
 	bool record_has_changed = record_i != handle->record_i;
 	handle->record_i = record_i;
 
 	// trigger pre triggers
-	if(handle->restored)
+	if(handle->restored || record_has_changed)
 	{
-		_trigger_open(handle);
+		if(record_i)
+			_trigger_open_record(handle);
+		else
+			_trigger_open_play(handle);
+
 		handle->restored = false;
 	}
 
 	lv2_atom_sequence_clear(handle->event_out);
 
+	handle->beats_period = handle->beats_upper;
+
 	int64_t last_t = 0;
 	LV2_ATOM_SEQUENCE_FOREACH(handle->event_in, ev)
 	{
-		if(handle->rolling)
-			handle->offset += ev->time.frames - last_t;
-
 		const LV2_Atom_Object *obj = (const LV2_Atom_Object *)&ev->body;
 		const int time_event = timely_advance(&handle->timely, obj, last_t, ev->time.frames);
+		handle->beats_upper = _beats(&handle->timely);
 
 		if(record_i && !time_event && handle->rolling)
 			_rec(handle, ev); // dont' record time position signals
@@ -390,9 +391,9 @@ run(LV2_Handle instance, uint32_t nsamples)
 		last_t = ev->time.frames;
 	}
 
-	if(handle->rolling)
-		handle->offset += nsamples - last_t;
 	timely_advance(&handle->timely, NULL, last_t, nsamples);
+	handle->beats_upper = _beats(&handle->timely);
+
 	if(!record_i && handle->rolling)
 		_play(handle, nsamples, capacity);
 
@@ -417,7 +418,7 @@ cleanup(LV2_Handle instance)
 		varchunk_free(handle->from_disk);
 
 	if(handle->io)
-		gzclose(handle->io);
+		fclose(handle->io);
 
 	free(handle);
 }
@@ -487,7 +488,7 @@ _state_restore(LV2_Handle instance, LV2_State_Retrieve_Function retrieve,
 		return LV2_STATE_ERR_BAD_TYPE;
 
 	if(!path)
-		path = "seq.gz";
+		path = DEFAULT_FILE_NAME;
 
 	const char *absolute = map_path->absolute_path(map_path->handle, path);
 
@@ -517,12 +518,11 @@ _work(LV2_Handle instance,
 	{
 		case JOB_PLAY:
 		{
-			//printf("play\n");
-			while(!gzeof(handle->io))
+			while(!feof(handle->io))
 			{
 				// read event header from disk
 				LV2_Atom_Event ev1;
-				if(gzread(handle->io, &ev1, sizeof(LV2_Atom_Event)) == sizeof(LV2_Atom_Event))
+				if(fread(&ev1, sizeof(LV2_Atom_Event), 1, handle->io) == 1)
 				{
 					size_t len = sizeof(LV2_Atom_Event) + ev1.body.size;
 					LV2_Atom_Event *ev2;
@@ -534,41 +534,99 @@ _work(LV2_Handle instance,
 						ev2->body.size = ev1.body.size;
 
 						// read event body from disk
-						if(gzread(handle->io, LV2_ATOM_BODY(&ev2->body), ev1.body.size) == (int)ev1.body.size)
+						if(ev1.body.size == 0)
+						{
+							//fprintf(stderr, "play: beats %lf %u\n", ev1.time.beats, ev1.body.size);
+							handle->seek_beats = ev1.time.beats;
 							varchunk_write_advance(handle->from_disk, len);
+						}
+						else if(fread(LV2_ATOM_BODY(&ev2->body), ev1.body.size, 1, handle->io) == 1)
+						{
+							//fprintf(stderr, "play: beats %lf %u\n", ev1.time.beats, ev1.body.size);
+							handle->seek_beats = ev1.time.beats;
+							varchunk_write_advance(handle->from_disk, len);
+						}
 						else
-							fprintf(stderr, "reading event body failed.\n");
+						{
+							if(!feof(handle->io))
+								fprintf(stderr, "play: reading event body failed.\n");
+						}
 					}
 					else
+					{
+						fseek(handle->io, -sizeof(LV2_Atom_Event), SEEK_CUR); // rewind
+
 						break; // ringbuffer full
+					}
 				}
 				else
-					fprintf(stderr, "reading event header failed.\n");
+				{
+					if(!feof(handle->io))
+						fprintf(stderr, "play: reading event header failed.\n");
+				}
 			}
 
 			break;
 		}
 		case JOB_RECORD:
 		{
-			//printf("record\n");
 			const LV2_Atom_Event *ev;
 			size_t len;
 			while((ev = varchunk_read_request(handle->to_disk, &len)))
 			{
-				if(gzwrite(handle->io, ev, len) == (int)len)
+				if(fwrite(ev, len, 1, handle->io) == 1)
+				{
+					//fprintf(stderr, "record: beats %lf %u\n", ev->time.beats, ev->body.size);
+					handle->seek_beats = ev->time.beats;
 					varchunk_read_advance(handle->to_disk);
+				}
 				else
-					fprintf(stderr, "writing event failed.\n");
+				{
+					fprintf(stderr, "record: writing event failed.\n");
+					break;
+				}
 			}
 
 			break;
 		}
 		case JOB_SEEK:
 		{
-			printf("seek: %lf\n", job->beats);
-			if(handle->io && (job->beats == 0.0) ) //FIXME
+			fprintf(stderr, "seek: request to %lf\n", job->beats);
+
+			// rewind to start of file
+			if(job->beats < handle->seek_beats)
 			{
-				gzseek(handle->io, 0, SEEK_SET);
+				fprintf(stderr, "seek rewind\n");
+				fseek(handle->io, 0, SEEK_SET);
+			}
+
+			while(!feof(handle->io))
+			{
+				// read event header from disk
+				LV2_Atom_Event ev1;
+				if(fread(&ev1, sizeof(LV2_Atom_Event), 1, handle->io) == 1)
+				{
+					// check whether event precedes current seek
+					if(ev1.time.beats < job->beats)
+					{
+						// skip event
+						if(ev1.body.size > 0)
+							fseek(handle->io, ev1.body.size, SEEK_CUR); //FIXME check return
+
+						continue;
+					}
+
+					// unread event header
+					fseek(handle->io, -sizeof(LV2_Atom_Event), SEEK_CUR); //FIXME check return
+					fprintf(stderr, "seek: seeked to %lf\n", ev1.time.beats);
+
+					break;
+				}
+				else
+				{
+					if(!feof(handle->io))
+						fprintf(stderr, "seek: reading event header failed.\n");
+				}
 			}
 
 			break;
@@ -580,27 +638,34 @@ _work(LV2_Handle instance,
 			const size_t len = sizeof(LV2_Atom_Event);
 			if((ev = varchunk_write_request(handle->from_disk, len)))
 			{
-				ev->time.beats = 0.0;
+				ev->time.beats = 0.0; //XXX
 				ev->body.type = handle->uris.orbit_drain;
 				ev->body.size = 0;
 
 				varchunk_write_advance(handle->from_disk, len);
 			}
 			else
-				fprintf(stderr, "ringbuffer overflow\n");
+				fprintf(stderr, "drain: ringbuffer overflow\n");
 
 			break;
 		}
-		case JOB_OPEN:
+		case JOB_OPEN_PLAY:
 		{
-			printf("open: %s\n", handle->path);
+			fprintf(stderr, "open for playback: %s\n", handle->path);
 			if(handle->io)
-			{
-				if(handle->io)
-					gzclose(handle->io);
-				if(!(handle->io = gzopen(handle->path, "r")))
-					fprintf(stderr, "failed to open file.\n");
-			}
+				fclose(handle->io);
+			if(!(handle->io = fopen(handle->path, "r+")))
+				fprintf(stderr, "failed to open file.\n");
+
+			break;
+		}
+		case JOB_OPEN_RECORD:
+		{
+			fprintf(stderr, "open for recording: %s\n", handle->path);
+			if(handle->io)
+				fclose(handle->io);
+			if(!(handle->io = fopen(handle->path, "w+")))
+				fprintf(stderr, "failed to open file.\n");
 
 			break;
 		}
